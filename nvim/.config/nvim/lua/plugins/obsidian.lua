a2cddff54d5dbb2bae7be0aa0c4e7dc577d04cbc
Roy Theimer <roy.theimer@gmail.com>
2024-08-21 09:26:14 +0200

    Added Modifications

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,16 +1,18 @@
 return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	--ft = "markdown",
+	lazy = false,
+	-- ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 	event = {
 		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
 		"BufReadPre "
-			.. vim.fn.expand("~/Dokumente/brain")
-			.. "/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
+			.. vim.fn.expand("~")
+			.. "/Dokumente/brain/**.md",
+		--"BufReadPre "
+		--	.. vim.fn.expand("~/Dokumente/brain")
+		--	.. "/**.md",
+		"BufNewFile ~/Dokumente/brain/**.md",
 	},
 	dependencies = {
 		-- Required.
@@ -25,9 +27,46 @@ return {
 		},
 		templates = {
 			folder = "templates",
-			date_format = "%Y-%m-%d-%a",
+			date_format = "%a-%d-%m-%Y",
 			time_format = "%H:%M",
 		},
+		picker = {
+			name = "telescope.nvim",
+		},
+		disable_frontmatter = false,
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+
+			-- Get the current date in the desired format (e.g., YYYY-MM-DD)
+			local current_date = os.date("%d-%m-%Y")
+
+			-- Initial out structure with basic fields and an empty 'author' and 'creation_date'
+			local out = {
+				id = note.id,
+				aliases = note.aliases,
+				tags = note.tags,
+				context = "",
+				source = "",
+				author = "", -- Set 'author' field to an empty string
+				creation_date = current_date, -- Set 'creation_date' to the current date
+			}
+
+			-- Keep manually added metadata in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			-- Return the extended out table with empty 'author' and 'creation_date'.
+			return out
+		end,
+
+		notes_subdir = "new_notes",
+		new_notes_location = "notes_subdir",
 		-- Optional, customize how note IDs are generated given an optional title.
 		---@param title string|?
 		---@return string
